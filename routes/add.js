3b352bb6ec635a275b1246b9b0efdc88c22a5ca4
katie redmond <katieredmond@dn0a2295ef.sunet>
2014-02-05 16:06:28 -0800

    lab 5

---
+++ routes/add.js
@@ -0,0 +1,11 @@
+var data = require("../data.json");
+
+exports.addFriend = function(req, res) {    
+	newFriend ={
+		'name':req.query.name, 
+		'description':req.query.description,
+		'imageURL': 'http://lorempixel.com/400/400/people'
+	};
+	data["friends"].push(newFriend);
+	res.render('add');
+ }
